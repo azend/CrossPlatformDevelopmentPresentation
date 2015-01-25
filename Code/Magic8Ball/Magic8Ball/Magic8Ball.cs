1017cf37b7ba40c7fa684604aab14da0289872e1
Verdi R-D <verdi@azend.org>
2015-01-25 18:36:30 -0500

    Created a magic 8 ball class to pull phrases from

---
+++ Code/Magic8Ball/Magic8Ball/Magic8Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Magic8Ball
+{
+	public class Magic8Ball
+	{
+		private static string[] phrases = new string[] {
+			"Hello world"
+		};
+
+		private Random r = new Random();
+
+		public Magic8Ball ()
+		{
+		
+		}
+
+		public string Shake() 
+		{
+			int i = r.Next (0, phrases.Length - 1);
+			return phrases [i];
+		}
+	}
+}
+
